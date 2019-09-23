d5a3786c8076323cd5bb452323fa4ef448b5b32d
Alejandro Tejada <tej17584@uvg.edu.gt>
2019-09-22 23:55:27 -0600

    Sección 5
    Hasta la sección 5

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -20,6 +21,14 @@ public class PlayerController : MonoBehaviour
     //Camara
     private Camera theCam;
 
+    //Playermodel
+    public GameObject playerModel;
+
+    //Velocidad de rotacion
+    public float rotateSpeed;
+
+    //Animacion
+    public Animator Anim;
 
     // Start is called before the first frame update
     void Start()
@@ -31,15 +40,25 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Se guarda el movimiento
         float yStore = moveDirection.y;
+        //Para que el jugador vea
+        moveDirection = (transform.forward * Input.GetAxisRaw("Vertical")) +
+                        (transform.right * Input.GetAxisRaw("Horizontal"));
         //Parte de la dirección del movimiento
-        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        //moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        //Normalizamos
+        moveDirection.Normalize();
         moveDirection *= moveSpeed;
         moveDirection.y = yStore;
-        //Hacemos el if por si salta 
-        if (Input.GetButtonDown("Jump"))
+        //Si esta en el suelo
+        if (charController.isGrounded)
         {
-            moveDirection.y = jumpForce;
+            //Hacemos el if por si salta 
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpForce;
+            }
         }
 
         moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
@@ -51,6 +70,13 @@ public class PlayerController : MonoBehaviour
         if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
         {
             transform.rotation = Quaternion.Euler(0f, theCam.transform.rotation.eulerAngles.y, 0f);
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+            //playerModel.transform.rotation = newRotation;
+            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation,
+                rotateSpeed * Time.deltaTime);
         }
+
+        Anim.SetFloat("Speed", Math.Abs(moveDirection.x) + Math.Abs(moveDirection.z));
+        Anim.SetBool("Grounded", charController.isGrounded);
     }
 }
